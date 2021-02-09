229b03d7cc3a62f2e2bb1804d2de821b9386e6a7
alejandramgt <alejandramgt@ibm.com>
2021-02-09 17:33:51 -0600

    Fixing models and test.

--- app/core/models.py
+++ app/core/models.py
@@ -12,7 +12,7 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-        
+
         return user
 
     def create_superuser(self, email, password):
@@ -21,7 +21,7 @@ class UserManager(BaseUserManager):
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
-        
+
         return user
 
 
@@ -31,7 +31,5 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     objects = UserManager()
-
     USERNAME_FIELD = 'email'
